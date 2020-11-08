15491efd0ecbb7751fe1823a0b619a325f821d46
Stacy Read <stacy.read.007@gmail.com>
2020-11-08 14:11:34 -0600

    Initial commit

---
+++ conditionals.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    // add the functions as event listeners
+    // to the forms in the HTML
+
+	function countClick() {
+		// Increment a variable that tracks the
+		// number of button clicks
+
+		// Print the current number of clicks to the
+		// <p> with ID "clickCountOutput"
+
+		// When the count gets to 10, reset it to 0
+
+
+
+	}
+
+
+    function checkAge() {
+        // Get the user's birth year from the text
+        // box with ID of "birthYear"
+
+        // If they are currently under 18, print "Child"
+        // to the <p> with ID of "birthYearOutput"
+
+        // If they are 18 or over, print "Adult" instead
+
+    }
+
+    function calcSalesTax() {
+        // Get the purchase amount from the text
+        // box with ID of "purchaseAmount"
+
+        // Get the state from the text box with ID "state"
+
+        // Tax rates are: WI 5%, IL 8%, MN 7.5%, MI 5.5%
+
+        // Calculate the sales tax amount and print to
+        // the <p> with ID of "salesTaxOutput"
+
+        // If the user enters a state not listed above,
+        // print "Error" instead
+
+
+    }
+
+    function recommendFood() {
+        // Get the cat's age from the text box with
+        // ID of "catAge"
+
+        // Cats under 2 get "kitten chow", between 2 and 10
+        // get "adult chow", and over 10 get "senior chow"
+
+        // Print the food recommendation to the <p> with
+        // ID of "catAgeOutput"
+
+
+    }
+
+    function drawCard() {
+        // Generate a random card face value (1 - 13)
+        var faceValue = Math.floor(Math.random() * 13) + 1;
+
+        // Generate a random suit (1 - 4)
+        var suit = Math.floor(Math.random() * 4) + 1;
+
+        // Create the description of the card, for example
+        // "King of Spades" or "2 of Hearts"
+        var description;
+
+        // For face values 2 - 10, you can just print the number
+        // Face value 1 is "Ace", 11 is "Jack", 12 is "Queen",
+        // and 13 is "King"
+
+        // For the suits, 1 is "Clubs", 2 is "Spades",
+        // 3 is "Hearts", 4 is "Diamonds"
+
+        // Print the card's description to the <p> with
+        // ID of "drawCardOutput"
+
+
+    }
+
+});
